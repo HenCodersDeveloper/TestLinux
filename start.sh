04e76ee281626164d99fa178027cf5fe713c66a4
HenCoders <lincolncowper@outlook.com>
2024-11-25 05:18:36 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,42 +1,37 @@
 #!/bin/bash
 
-# Menyiapkan koneksi RDP
+# Mengonfigurasi RDP
 echo "Mengonfigurasi RDP untuk pengguna 'administrator'..."
-
-# Menambahkan pengguna administrator dan password
 sudo useradd -m administrator
 echo "administrator:HenCoders2024" | sudo chpasswd
 sudo usermod -aG sudo administrator
 
-# Mengaktifkan dan mengonfigurasi xRDP
-echo "Mengonfigurasi xRDP..."
+# Mengaktifkan xRDP
+echo "Mengaktifkan xRDP..."
 sudo systemctl enable xrdp
 sudo systemctl start xrdp
-
-# Mengatur sesi default ke xfce4 untuk administrator
-sudo -u administrator bash -c "echo 'xfce4-session' > /home/administrator/.xsession"
-sudo chown administrator:administrator /home/administrator/.xsession
-
-# Menyesuaikan konfigurasi xRDP untuk xfce4
-sudo sed -i.bak '/. /c\startxfce4' /etc/xrdp/startwm.sh
+echo "xfce4-session" > ~/.xsession
 sudo systemctl restart xrdp
 
-# Menghubungkan ke Ngrok
+# Mengonfigurasi Ngrok
 echo "Menghubungkan ke Ngrok..."
 ./ngrok authtoken $NGROK_AUTH_TOKEN
-nohup ./ngrok tcp 3389 &> ngrok.log &
+sleep 5
+nohup ./ngrok tcp --region ap 3389 &> ngrok.log &
 sleep 10
 
-# Menampilkan informasi Ngrok (IP dan port)
-NGROK_INFO=$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
-if [ -z "$NGROK_INFO" ]; then
-  echo "Gagal mendapatkan informasi Ngrok. Pastikan token valid."
+# Mengecek dan Menampilkan URL Ngrok
+NGROK_URL=$(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url)
+if [[ $NGROK_URL == null ]]; then
+    echo "Gagal mendapatkan informasi Ngrok. Pastikan token valid."
+    cat ngrok.log
+    exit 1
 else
-  echo "============================================"
-  echo "RDP setup completed successfully!"
-  echo "Username: administrator"
-  echo "Password: HenCoders2024"
-  echo "Connect to RDP using the following address:"
-  echo "$NGROK_INFO"
-  echo "============================================"
+    echo "============================================"
+    echo "RDP setup completed successfully!"
+    echo "Username: administrator"
+    echo "Password: HenCoders2024"
+    echo "Connect to RDP using the following address:"
+    echo "$NGROK_URL"
+    echo "============================================"
 fi
